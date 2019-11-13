0161cf9c50f1a70cc0130d508b6082b6f2cec86d
coltereksonsems <56939690+coltereksonsems@users.noreply.github.com>
2019-11-13 14:57:44 -0800

    module 1

---
+++ src/_02_unbirthday/UNbirthday.java
@@ -0,0 +1,53 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class UNbirthday {
+public static void main(String[] args) {
+	String q1 = JOptionPane.showInputDialog("when is your birthday?");
+	if (q1.equals("11/13/2019")) {
+		JOptionPane.showMessageDialog(null,"happy birthday!");
+	}
+		else {
+			JOptionPane.showMessageDialog(null,"merry UNbirthday!");
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
