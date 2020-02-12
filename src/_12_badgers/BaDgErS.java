90363a2fce9a6c3d779a00dfc9505885e4507845
coltereksonsems <56939690+coltereksonsems@users.noreply.github.com>
2020-02-12 14:55:41 -0800

    for loops and if statements

---
+++ src/_12_badgers/BaDgErS.java
@@ -0,0 +1,19 @@
+package _12_badgers;
+
+public class BaDgErS {
+public static void main(String[] args) {
+	
+	
+	for (int i = 0; i < 11; i++) {
+		
+		System.out.println("badger");
+	}
+		for (int j = 0; j < 2; j++) {
+			System.out.println("mushroom");
+		}
+	for (int i = 0; i < 5; i++) {
+		System.out.println("snake");
+	}
+	
+}
+}
