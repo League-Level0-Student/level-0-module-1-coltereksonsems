b84e28c06651ce104ee142c60a804287566e19e3
coltereksonsems <56939690+coltereksonsems@users.noreply.github.com>
2020-02-19 14:55:53 -0800

    almost done with lv 1

---
+++ src/_13_bananas_4eva/banannas4ever.java
@@ -0,0 +1,22 @@
+package _13_bananas_4eva;
+
+public class banannas4ever {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 1000; i++) {
+			System.out.println("bananas");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/_14_banana_split/banana_split/bananasplit.java
@@ -0,0 +1,28 @@
+package _14_banana_split.banana_split;
+
+public class bananasplit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.println("ice cream");
+		}
+		System.out.println("banana");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
